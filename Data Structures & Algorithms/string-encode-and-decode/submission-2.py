f1713ea43e5ecdba4ea0f9d850892943316b1458
spolisar <spolisar@users.noreply.github.com>
2026-04-29 14:43:50 -0400

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,30 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        sizes = [len(s) for s in strs]
+        sizeStr = '#'.join([str(s) for s in sizes])
+        # print(sizeStr)
+        wordStr = ''.join(strs)
+        result = sizeStr + '\0' + wordStr
+        # print(result.split('\0'))
+        return result
+        
+        
+
+    def decode(self, s: str) -> List[str]:
+        if len(s) == 0:
+            return []
+        sizeWordSplit = s.split('\0')
+        sizeStr = sizeWordSplit[0]
+        wordStr = sizeWordSplit[1]
+        sizeStrings = sizeStr.split('#')
+        i = 0
+        result = []
+        for size in sizeStrings:
+            length = int(size)
+            word = wordStr[i:i+length]
+            i += length
+            result.append(word)
+        return result
